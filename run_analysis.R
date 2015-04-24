305a190d5eb788ddc7ca50518ac7adb90ae5b407
Li <ekinasm@gmail.com>
2015-04-24 21:51:57 +0800

    1.5

--- run_analysis.R
+++ run_analysis.R
@@ -6,42 +6,36 @@
   
   # 1.1 get names for train data
   features <- read.table("./UCI HAR Dataset/features.txt", header=FALSE)
-  names(train_x) <- features$V2
+  feature_names <- paste(features$V1,features$V2)
+  names(train_x) <- feature_names
   
-  # 1.2 validate names
-  valid_column_names <- make.names(names=names(train_x), unique=TRUE, allow_ = TRUE)
-  names(train_x) <- valid_column_names
-  
-  # 1.3 read train_y
+  # 1.2 read train_y
   train_y <- read.table("./UCI HAR Dataset/train/y_train.txt", header=FALSE)
   names(train_y) <- "activity"
   
-  #1.4 uses descriptive for activity names
+  #1.3 uses descriptive for activity names
   activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", header=FALSE)
   activity_labels <- as.matrix(activity_labels)  
   train_y <- mutate(train_y, activity = activity_labels[activity,2])
   
-  # 1.5 read subject data and get names
+  # 1.4 read subject data and get names
   subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", header=FALSE)
   names(subject_train) <- "subject"
   
 # 2. read test data
   test_x <- read.table("./UCI HAR Dataset/test/X_test.txt", header=FALSE)
   
-  # 1.1 get names for train data
-  names(test_x) <- features$V2
+  # 2.1 get names for train data
+  names(test_x) <- feature_names
   
-  # 1.2 validate names
-  names(test_x) <- valid_column_names
-  
-  # 1.3 read train_y
+  # 2.2 read train_y
   test_y <- read.table("./UCI HAR Dataset/test/y_test.txt", header=FALSE)
   names(test_y) <- "activity"
   
-  #1.4 uses descriptive for activity names
+  # 2.3 uses descriptive for activity names
   test_y <- mutate(test_y, activity = activity_labels[activity,2])
   
-  # 1.5 read subject data and get names
+  # 2.4 read subject data and get names
   subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", header=FALSE)
   names(subject_test) <- "subject"
   
@@ -58,4 +52,5 @@
     total_record %>%
     group_by(activity, subject) %>%
     summarise_each(funs(mean))
-  
+# replace the number in names   
+  names(results) <- gsub("[0-9]", "", names(results))
